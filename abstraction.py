ee75c84f44e0a8883cdfd0c8f089ba6b0f1d7ab1
Lola-n28 <lola-n@mail.ru>
2021-05-10 00:00:24 +0600

    abstraction

---
+++ abstraction.py
@@ -0,0 +1,11 @@
+class Table:
+    model = 'Kitchen'
+    vendor = 'Ikea'
+    size = 150
+    surface = 'wood'
+    price = 5000
+    weight = 20
+
+
+table = Table()
+print(table.model, table.price)
