dfa9b17c418ae33d5e6c17fa0d26c840de176e49
Lola-n28 <lola-n@mail.ru>
2021-05-10 01:07:08 +0600

    class

---
+++ class.py
@@ -0,0 +1,13 @@
+class Computer:
+    hdd = 1000
+    ram = 16
+    model = "HP 14133"
+    power = 100
+    is_working = False
+
+def turn_on(self):
+    self.isworking = True
+    self.power -= 1
+
+hp_1 = Computer()
+print(hp_1.ram)
